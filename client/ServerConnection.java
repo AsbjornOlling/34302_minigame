90d5b6864789c0e2f9ce241bc2c979caa4ee5f19
AsbjornOlling <asbjornolling@gmail.com>
2018-01-04 14:28:39 +0100

    exceptions weeeee

--- client/ServerConnection.java
+++ client/ServerConnection.java
@@ -12,9 +12,11 @@ import java.io.BufferedReader;
 import java.io.PrintWriter;
 
 public class ServerConnection {
+	// server address
 	private final String HOST = "localhost";
-	private final int PORT = "6666";
+	private final int PORT = 6666;
 
+	// connection objects
 	private Socket connection;
 	private BufferedReader in;
 	private PrintWriter out;
@@ -24,25 +26,40 @@ public class ServerConnection {
 		connect();
 	} // constructor
 
+
+	// init connection objects
+	public void connect() {
+		try { // connect to server
+			connection = new Socket(HOST, PORT);
+		} catch (Exception ex) {
+			// tell the user that he dun goof
+		} 
+		
+		try { // input reader and output writer
+			in = new BufferedReader(
+					 new InputStreamReader(connection.getInputStream()));
+			out = new PrintWriter(connection.getOutputStream());
+		} catch (Exception ex) {
+			// tell the user that he dun goof
+		}
+	} // connect
+
+
+	// send and receive data
 	public void update() {
 		// send score
 		// read scores of other players
-	}
+	} // update
 
-	public void connect() {
-		// make connection objects
-		connection = new Socket(HOST, PORT);
-		in = new BufferedReader(
-				 new InputStreamReader(connection.getInputStream()));
-		out = new PrintWriter(connection.getOutputStream());
-	}
 
 	// join a game session
 	public void joinSession(String sessionid) {
-	}
+		// send player name
+	} // joinSession
+
 
 	// make a new game session
 	public void createSession() {
-	}
+	} // createSession
 
 } // class
