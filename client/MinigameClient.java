7dc6ca3213674d3cee67489c1151222189d68141
AsbjornOlling <asbjornolling@gmail.com>
2018-01-04 14:36:45 +0100

    initGUI method

--- client/MinigameClient.java
+++ client/MinigameClient.java
@@ -11,20 +11,27 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class MinigameClient extends JFrame implements ActionListener {
-
 	ServerConnection server;
 	
 	// constructor
 	public MinigameClient() {
 		// init gui stuff
+		initGUI();
+
 		// init ServerConnection
 		server = new ServerConnection();
+
 		// init GameHandler
 	} // constructor
 
 	// event handler
 	public void actionPerformed(ActionEvent e) {
 	} // event handler
+
+	public void initGUI() {
+		getContentPane().setLayout(new GridLayout(2, 1));
+
+	} // initGUI
 
 	public static void main(String[] args) {
 		MinigameClient client = new MinigameClient();
