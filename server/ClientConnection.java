c38413316513c4d643bf01c737291079e7fa9135
DaniliJog <Danili.jog@gmail.com>
2018-01-05 12:51:19 +0100

    Maybe ready for test now

--- server/ClientConnection.java
+++ server/ClientConnection.java
@@ -1,3 +1,9 @@
+/*
+ * Handels connection from the game client
+ * Reads the incomming packages from the game client
+ * 
+ */
+
 import java.io.*;
 import java.util.ArrayList;
 import java.net.Socket;
@@ -6,25 +12,26 @@ import java.nio.file.*;
 
 public class ClientConnection {
 	boolean serverActive = true; // For the MinigamesServer main file
+	boolean DEBUG = true; // Debug parameter
 
 	// Socket fields
 	ServerSocket serverSocket;
 	Socket clientSocket;
 
-	// Constructor
+	// Constructor that makes the server listen
 	public ClientConnection(int port) {
 		try { 
 			serverSocket = new ServerSocket(port);
 		} catch (IOException ioEx) {
-			System.out.println("ERROR: Could not establish the connection");
+			System.out.println("ERROR: Could not establish socket on port "+port);
 		}
 	} // Constructor
 
 	// Wait for the next incomming package
 	// and put it into an arrayList
-	public Arraylist<String> inPackage() {
+	public ArrayList<String> inPackage() {
 		BufferedReader input = null;
-		ArrayList<String> package = new ArrayList<String>();
+		ArrayList<String> request = new ArrayList<String>();
 		
 		// wait for client to connect
 		try { 
@@ -50,26 +57,8 @@ public class ClientConnection {
 		return request;
 	} //inPackage
 
-	// send a bytearray back
-	public void sendResponse(Response response) {
-		// open output stream
-		BufferedOutputStream output = null;
-		try { 
-			output = new BufferedOutputStream(clientSocket.getOutputStream());
-		} catch (IOException ioEx) {
-			System.out.println("ERROR: Could not open outputstream to client.");
-		}
-
-		// write some shit to client
-		// EDIT SHIT L8R
-		try { 
-			output.write( response.SHIT);
-			if ( DEBUG ) System.out.print(response.SHIT);
-
-			output.flush();
-		} catch (IOException ioEx) {
-			System.out.println("ERROR: Could not write data to outputstream.");
-		}
+	// Close the server socket
+	public void closeSocket() {
 
 		// close the client connection
 		try {
@@ -78,7 +67,7 @@ public class ClientConnection {
 			System.out.println("ERROR: Could not close connection to client.");
 		} //*/
 
-	} // sendResponse
+	} // CloseSocket
 
 
 } // Class
--- server/MinigameServer.java
+++ server/MinigameServer.java
@@ -3,7 +3,7 @@
  * MiniGames server part
  */
 
-// Importing important stuff
+// Importing important imports
 import java.io.*;
 import java.util.*;
 import java.net.*;
@@ -12,22 +12,25 @@ public class MinigameServer {
 	
 		public static void main(String[] args) {
 		
-		ClientConnection connection = new ClientConnection(6666);
+			// Calling the cunstructor in ClientConnection
+			ClientConnection connection = new ClientConnection(6666);
 
-		// Recives packages from client
-		// as long as the server is active
-		// This is permanent right now
-		while (connection.serverActive == true) {
+			// Recives packages from client
+			// as long as the server is active
+			// This is permanent right now
+			while (connection.serverActive == true) {
 			
-			// Open socket and wait for package to arrive
-			ArrayList<String> incommingPackage = connection.inPackage();
+				// Open socket and wait for package to arrive
+				ArrayList<String> incommingPackage = connection.inPackage();
 
-			// Gonna call the the GameSession when that is made
-
-		}
-		
-		System.out.println("Connection closed");
-		// Connection closed
+				// Gonna call the the GameSession when that is made
+				System.out.println("I should contact GameSession now");
+			}
+			
+			connection.closeSocket();
+				
+			System.out.println("Connection closed");
+			// Connection closed
 
 	} // Main
 } // Class
