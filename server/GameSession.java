23032922cb49cb701cee64c3a56f5bb128439175
AsbjornOlling <asbjornolling@gmail.com>
2018-01-08 21:59:36 +0100

    gamesession elaboration - session id generation

--- server/GameSession.java
+++ server/GameSession.java
@@ -1,18 +1,59 @@
-/*
- * Handels the entire game session
- * The score and the players
- * Game Game Game
- *
- * Blame David for any shit in here
+/* 
+ * Class for gameSession objects:
+ * A gamesession object contains 
+ * 	any number of clients
+ * 	a list of games to play
+ * 	a score table
  */
 
 
+import java.util.*;
+import java.io.*;
+
 public class GameSession {
+	MinigameServer parent;
 
-	// Constructor to make a game
+	Random r; // randomizer
+
+	ArrayList<Object> clients;					// all connected clients
+	HashMap<String,Integer> scoreboard; // scoreboard...
+	Integer[] gamesList;								// list of game ids to play
+	String sessionID;
+
+	int gameLength = 1; 				// number of minigames to play
+	boolean lobbyMode = true;		// allow other players to join
 
 
+	// constructor
+	public GameSession(MinigameServer parent) {
+		this.parent = parent;
+
+		// make randomizer
+		r = new Random();
+
+		// init lists
+		clients = new ArrayList<Object>();
+		scoreboard = new HashMap<String,Integer>();
+
+		// generate session ID
+		sessionID = genSessionID();
+		System.out.println("SESSION MADE: "+sessionID);
+	} // constructor
 
 
+	// generate session ID from wordlist
+	public String genSessionID() {
+		int idLength = 3;
+		String sessionID = "";
+		// get some random words from the wordlist
+		for (int i = 0; i < idLength; i++) {
+			sessionID += parent.wordlist[r.nextInt(parent.wordlist.length)];
+			// add space if not the last word
+			if (i + 1 < idLength) {
+				sessionID += " ";
+			}
+		}
+		return sessionID;
+	} // genSessionID
 
-}
+} // GameSession
--- server/MinigameServer.java
+++ server/MinigameServer.java
@@ -1,38 +1,56 @@
 /*
  * This is the main class of the 
- * MiniGames server part
+ * Minigames Server
+ *
+ * Mainly for instantiating the different main objects
  */
+
+import java.io.*;
+import java.util.*;
 
 public class MinigameServer {
 	// constant params
 	public final int PORT = 6666;
 
+	// wordlist for session IDs
+	String[] wordlist;
+
 	// main objects
 	Listener newConnects;
-	GameState state;
 
 	// constructor
 	public MinigameServer() {
+		// init wordlist array
+		try { loadWordList(); } catch (Exception e) {
+			System.out.println("ERROR: Could not read wordlist");
+		}
+
 		// make listener object
 		newConnects = new Listener(this);
-		// start thread
 		Thread cThread = new Thread(newConnects);
 		cThread.start();
 
-		// make gamestate object
-		state = new GameState(this);
+		GameSession s = new GameSession(this);
 	} // constructor
 
-	// temp testing loop
-	public void deleteMe() {
-		while (true) {
-			state.printAllPackets();
+
+	// load list of words into String[] wordlist
+	public void loadWordList() throws IOException {
+		FileReader fr = new FileReader("wordlist.txt");
+		BufferedReader br = new BufferedReader(fr);
+		ArrayList<String> lines = new ArrayList<String>();
+		String line = null;
+
+		while ((line = br.readLine()) != null) {
+			lines.add(line);
 		}
-	}
-	
+		wordlist = lines.toArray(new String[lines.size()]);
+	} // loadWordList
+
+
 	// main
 	public static void main(String[] args) {
 		MinigameServer serverInstance = new MinigameServer();
-		serverInstance.deleteMe();
+
 	} // Main
 } // Class
