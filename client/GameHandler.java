af83db03d42bf6cd04a3775792ce553146653efa
AsbjornOlling <asbjornolling@gmail.com>
2018-01-08 15:01:14 +0100

    a bunch of progress on gamehandler and speedClick proto-game

--- client/GameHandler.java
+++ client/GameHandler.java
@@ -11,29 +11,46 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class GameHandler extends JPanel {
-	int WIDTH, HEIGHT;
+	final int WIDTH, HEIGHT;
 
 	// constructor
 	public GameHandler(int WIDTH, int HEIGHT) {
 		this.WIDTH = WIDTH;
 		this.HEIGHT = HEIGHT;
 
-		// just for testing - a box with the full dimensions
-		this.setLayout(new GridLayout(1,1));
-		this.add(Box.createRigidArea(new Dimension(WIDTH, HEIGHT)));
+		this.setLayout(new GridLayout(1, 1));
+
+		// testing - load button game
+		this.loadGame(new ClickTenTimes(this));
 	} // constructor
 
 
-	// construct and display a minigame
+	// display a minigame
 	public void loadGame(Game game) {
+		// clear screen and add new game
 		this.removeAll();
 		this.add(game);
-	} // Game
 
+		// start game thread
+		Thread t = new Thread(game);
+		t.start();
+	} // loadGame
+
+
+	// send gameComplete message
+	// and load the next game
+	public void gameComplete(int score) {
+		loadGame(new ClickTenTimes(this));
+	} // gameComplete
+
+
+	// return gamewindow dimension
 	public Dimension getPreferredSize() {
 		return new Dimension(WIDTH, HEIGHT);
 	} // getPreferredSize
 
+
+	// do something, idk: this one is required
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 	} // paintComponent
@@ -41,29 +58,115 @@ public class GameHandler extends JPanel {
 
 
 // parent class for all minigames
-abstract class Game extends JPanel {
+abstract class Game extends JPanel implements Runnable, ActionListener {
+	GameHandler handler;
+
 
 	// constructor
-	public Game() {
+	public Game(GameHandler handler) {
+		this.handler = handler;
 	} // constructor
 
+
+	// return full size of handler
 	public Dimension getPreferredSize() {
-		return new Dimension(WIDTH, HEIGHT);
+		return new Dimension(handler.WIDTH, handler.HEIGHT);
 	} // getPreferredSize
 } // Game
 
 
-class ButtonGame extends Game {
+// ClickTenTimes: A game about a button
+class ClickTenTimes extends Game {
+	boolean shouldRun;
+	JButton tenClickButton;
+	Integer clicksLeft;
+	long startTime;
+
+
 	// constructor 
-	public ButtonGame() {
+	public ClickTenTimes(GameHandler handler) {
+		super(handler);
+
+		// set simple layout
+		this.setLayout(new GridLayout(3, 3));
+
+		// eight empty spaces and one button
+		for (int i = 0; i < 9; i++) {	
+			if (i != 4) {
+				// add empty spacer
+				add(Box.createRigidArea(
+						new Dimension(handler.WIDTH/3, handler.HEIGHT/3)));
+			} else {
+				// place button in center field
+				tenClickButton = new JButton();
+				tenClickButton.addActionListener(this);
+				add(tenClickButton);
+			}
+		} // grid loop
+
+		// init button
+		clicksLeft = 10;
+		update();
+
+		// get time and start game
+		startTime = System.currentTimeMillis();
+		shouldRun = true;
 	} // constructor
+
+
+	// thread loop
+	public void run() {
+		while (shouldRun) {
+			update();
+		} 
+		finishGame();
+	} // thread loop
+
+
+	// event listener
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == tenClickButton) {
+			clicksLeft--;
+		}
+	} // event listener
+
+
+	// update button state
+	public void update() {
+		tenClickButton.setText(clicksLeft.toString());
+
+		// check for terminating gamestate
+		if (clicksLeft <= 0) {
+			shouldRun = false;
+		} 
+	} // setButtonText
+
+
+	// calculate and send score
+	public void finishGame() {
+		// calculate time to complete
+		long endTime = System.currentTimeMillis();
+		long timePassed = endTime - startTime;
+		System.out.println("GAME COMPLETED IN: "+timePassed);
+		
+		// calculate score TODO make score of max 100
+		int score = (int) timePassed;
+		System.out.println("SCORE GOT: "+score);
+
+		// send gamescore, load new game
+		handler.gameComplete(score);
+	} // finishGame
 } // ButtonGame
+
 
 
    /*\
    ***
    *** 
    ***
-   ***
- *******
+   *** 
+   *** 
+   *** 
+/*******\
 \*******/
+
