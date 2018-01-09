f8b621cf5777f99b0a3f0212dd7e211cd95ea5c7
AsbjornOlling <asbjornolling@gmail.com>
2018-01-09 12:42:13 +0100

    Packet cosntructor fleshed out

---
+++ server/Packet.java
@@ -0,0 +1,25 @@
+// a packet object for easier access to the different values
+// only for received packets
+
+public class Packet {
+	public final String HEADER;
+	public final String PNAME;
+	public final String SESSIONID;
+
+	// for GAMECOMPLETE packets
+	public final int GSCORE;
+
+	// constructor
+	public Packet(String[] packetArray) {
+		HEADER = packetArray[0];
+		PNAME = packetArray[1].replace("PNAME: ", "");
+		SESSIONID = packetArray[2].replace("SESSIONID: ", "");
+
+		if (HEADER.equals("GAMECOMPLETE")) {
+			String score = packetArray[3].replace("GSCORE: ", "");
+			GSCORE = Integer.parseInt(score);
+		} else {
+			GSCORE = 0;
+		}
+	} // constructor
+} // Packet
