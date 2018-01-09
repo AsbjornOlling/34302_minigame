e3f31d32db194bcf7e5ffbbd1a2d3e1645bdba92
AsbjornOlling <asbjornolling@gmail.com>
2018-01-09 16:57:34 +0100

    string arrays containing packet generation texts

--- server/GameSession.java
+++ server/GameSession.java
@@ -6,7 +6,6 @@
  * 	a score table
  */
 
-
 import java.util.*;
 import java.io.*;
 
@@ -15,12 +14,26 @@ public class GameSession extends PacketListener {
 
 	Random r; // randomizer
 
+	String[] SESSIONJOINED = {
+		"SESSIONJOINED",
+		"GAMES: ",
+		"SESSIONID: ",
+		"END"
+	};
+
+	String[] SCOREUPDATE = {
+		"SCOREUPDATE",
+		"PNAME: ",		// repeat these two lines for number of players
+		"PSCORE: ",		// repeat these two lines for number of players
+		"END"
+	};
+
 	Client host;												// creator of the session
 	ArrayList<Client> clients;					// all connected clients
-	HashMap<String,Integer> scoreboard; // a atable of points (PName:Points))
+	HashMap<String,Integer> scoreboard; // a table of points (PName:Points)
 	Integer[] gamesList;								// list of game ids to play
 
-	String sessionID;						// id for players to join with
+	String sessionID;						// id for players to join
 	int gameLength = 1; 				// number of minigames to play
 	boolean inLobby = true;			// allow other players to join
 
@@ -35,6 +48,7 @@ public class GameSession extends PacketListener {
 		// init lists
 		clients = new ArrayList<Client>();
 		scoreboard = new HashMap<String,Integer>();
+		gamesList = new Integer[gameLength];
 
 		// add host
 		this.host = host;
@@ -47,7 +61,8 @@ public class GameSession extends PacketListener {
 		// put into main session register
 		parent.sessions.put(sessionID, this);
 		// add to mediator
-		String[] hdrs = {"SESSIONCONNECT"};
+		String[] hdrs = {"SESSIONCONNECT", 
+										 "GAMECOMPLETE"};
 		Mediator.getInstance().addListener(this, hdrs, sessionID);
 
 		// TODO generate gamesList
@@ -59,6 +74,10 @@ public class GameSession extends PacketListener {
 		if (pck.HEADER.equals("SESSIONCONNECT")
 				&& pck.SESSIONID.equals(this.sessionID)) {
 			addClient(pck.SOURCE);
+		}
+		else if (pck.HEADER.equals("GAMECOMPLETE")
+						 && pck.SESSIONID.equals(this.sessionID)) {
+			// TODO broadcastScore()
 		}
 	} // recvPacket
 
@@ -72,6 +91,8 @@ public class GameSession extends PacketListener {
 
 			System.out.println("PLAYER " + client.pName 
 												 + "JOINED SESSION " + sessionID);
+
+			// TODO sessionjoin packet
 		} else {
 			System.out.println("SESSION NOT JOINABLE");
 		}
